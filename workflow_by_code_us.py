685f2d111548bb2ace9682ec0ae031f813d90e40
gcsong <1586354782@qq.com>
2025-09-03 19:43:28 +0800

    修改为美股默认数据，在ipynb中生成分析图表

---
+++ workflow_by_code_us.py
@@ -0,0 +1,130 @@
+#  Copyright (c) Microsoft Corporation.
+#  Licensed under the MIT License.
+"""
+Qlib provides two kinds of interfaces.
+(1) Users could define the Quant research workflow by a simple configuration.
+(2) Qlib is designed in a modularized way and supports creating research workflow by code just like building blocks.
+
+The interface of (1) is `qrun XXX.yaml`.  The interface of (2) is script like this, which nearly does the same thing as `qrun XXX.yaml`
+"""
+import qlib
+from qlib.constant import REG_US
+from qlib.utils import init_instance_by_config, flatten_dict
+from qlib.workflow import R
+from qlib.workflow.record_temp import SignalRecord, PortAnaRecord, SigAnaRecord
+from qlib.tests.data import GetData
+
+
+if __name__ == "__main__":
+    # use default data
+    provider_uri = "./qlib_data/us_data"  # target_dir
+    GetData().qlib_data(target_dir=provider_uri, region=REG_US, exists_skip=True)
+    qlib.init(provider_uri=provider_uri, region=REG_US)
+
+    # 股票池 标普500
+    SP500_INSTRUMENTS = "sp500"
+    # 自定义一个适用于 SP500 的任务配置
+    SP500_GBDT_TASK = {
+        "model": {
+            "class": "LGBModel",
+            "module_path": "qlib.contrib.model.gbdt",
+            "kwargs": {
+                "loss": "mse",
+                "colsample_bytree": 0.8879,
+                "learning_rate": 0.0429,
+                "subsample": 0.8789,
+                "lambda_l1": 205.97,
+                "lambda_l2": 580.93,
+                "max_depth": 8,
+                "num_leaves": 210,
+                "num_threads": 20,
+            },
+        },
+        "dataset": {
+            "class": "DatasetH",
+            "module_path": "qlib.data.dataset",
+            "kwargs": {
+                "handler": {
+                    "class": "Alpha158",
+                    "module_path": "qlib.contrib.data.handler",
+                    "kwargs": {
+                        "instruments": SP500_INSTRUMENTS,  # 使用 SP500 股票池
+                        "start_time": "2008-01-01",
+                        "end_time": "2020-08-01",
+                        "fit_start_time": "2008-01-01",
+                        "fit_end_time": "2014-12-31",
+                    },
+                },
+                "segments": {
+                    "train": ("2008-01-01", "2014-12-31"),
+                    "valid": ("2015-01-01", "2016-12-31"),
+                    "test": ("2017-01-01", "2020-08-01"),
+                },
+            },
+        },
+    }
+
+    # 基准指数：SP500
+    SP500_BENCH = "^GSPC"
+
+    model = init_instance_by_config(SP500_GBDT_TASK["model"])
+    dataset = init_instance_by_config(SP500_GBDT_TASK["dataset"])
+
+    port_analysis_config = {
+        "executor": {
+            "class": "SimulatorExecutor",
+            "module_path": "qlib.backtest.executor",
+            "kwargs": {
+                "time_per_step": "day",
+                "generate_portfolio_metrics": True,
+            },
+        },
+        "strategy": {
+            "class": "TopkDropoutStrategy",
+            "module_path": "qlib.contrib.strategy.signal_strategy",
+            "kwargs": {
+                "signal": (model, dataset),
+                "topk": 50,
+                "n_drop": 5,
+            },
+        },
+        "backtest": {
+            "start_time": "2017-01-01",
+            "end_time": "2020-08-01",
+            "account": 100000000,
+            "benchmark": SP500_BENCH,
+            "exchange_kwargs": {
+                "freq": "day",
+                "limit_threshold": 0.095,
+                "deal_price": "close",
+                "open_cost": 0.0005,
+                "close_cost": 0.0015,
+                "min_cost": 5,
+            },
+        },
+    }
+
+    # NOTE: This line is optional
+    # It demonstrates that the dataset can be used standalone.
+    example_df = dataset.prepare("train")
+    print(example_df.head())
+
+    # start exp
+    with R.start(experiment_name="workflow"):
+        R.log_params(**flatten_dict(SP500_GBDT_TASK))
+        model.fit(dataset)
+        R.save_objects(**{"params.pkl": model})
+
+        # prediction
+        recorder = R.get_recorder()
+        sr = SignalRecord(model, dataset, recorder)
+        sr.generate()
+
+        # Signal Analysis
+        sar = SigAnaRecord(recorder)
+        sar.generate()
+
+        # backtest. If users want to use backtest based on their own prediction,
+        # please refer to https://qlib.readthedocs.io/en/latest/component/recorder.html#record-template.
+        par = PortAnaRecord(recorder, port_analysis_config, "day")
+        par.generate()
